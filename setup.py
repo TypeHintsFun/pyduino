3ebb4b33ec1d0efc54f95dc6b636752f214d02b7
TypeHintsFun <michailsmnv22@gmail.com>
2022-12-27 18:39:18 +0800

    preparing for pypi

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pyduino',
+      version='0.7',
+      description='A library for comfortable, fast and modern control of Arduino boards using python',
+      packages=['pyduino'],
+      install_requires=['pyserial'],
+      author_email='mikhailsmnv22@gmail.com',
+      zip_safe=False)
